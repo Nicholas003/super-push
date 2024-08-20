5dc896b7fbe018842c03401f1bb74055198f4220
e_liuzijing <e_liuzijing@enn.cn>
2024-08-20 16:44:19 +0800

    feat✨: 处理action

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode})=>{
+
+  const env = loadEnv(mode,'./')
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
