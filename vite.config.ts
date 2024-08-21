c6542e19f94eea2cf6c46c8e2d418dfe75c9e18f
Kovi <lzj4103@126.com>
2024-08-21 12:30:27 +0800

    feat():试一下pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -12,6 +12,20 @@ export default defineConfig(({mode})=>{
       vue(),
       vueJsx(),
       vueDevTools(),
+      VitePWA({
+        manifest: {
+          name: 'Vue 3 + Vite',
+          short_name: 'Vite + Vue 3',
+          description: 'Vite + Vue 3 + Vue Router + TypeScript + Tailwind CSS',
+          icons: [
+            {
+              src: '/icon.png',
+              sizes: '192x192',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
     ],
     resolve: {
       alias: {
